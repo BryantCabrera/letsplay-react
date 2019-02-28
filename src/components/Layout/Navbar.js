4cf7bdf7ca44035df0ac061061a940f0145e3f69
Abraham Hsu <smokiebacon@gmail.com>
2019-02-28 08:55:16 -0800

    adds card mapping

--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -14,7 +14,6 @@ class Navbar extends Component {
   // }
 
   render() {
-    console.log(this.props.loggedUser, 'navbar props');
     return (
       <div>
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
