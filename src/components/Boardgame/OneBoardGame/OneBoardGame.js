1ded329b64aa5d12b018ce1fef5c5680797b4131
Abraham Hsu <smokiebacon@gmail.com>
2019-02-28 17:53:15 -0800

    add individual boardgame page

--- src/components/Boardgame/OneBoardGame/OneBoardGame.js
+++ src/components/Boardgame/OneBoardGame/OneBoardGame.js
@@ -1,11 +1,45 @@
-import React from 'react'
+import React,{ Component } from 'react'
 
-const OneBoardGame = () => {
+class OneBoardGame extends Component {
+  state={
+    boardGame:  {}
+  }
+  componentDidMount(){
+    const { id } = this.props.match.params
+    this.getBoardGame(id)
+  }
+
+  getBoardGame = async (id) => {
+    try {
+      const response = await fetch(`http://localhost:8000/api/v1/boardgames/${id}`, {
+        method: 'GET',
+        credentials: 'include',
+        // mode: 'no-cors', do NOT need
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      if(!response.ok) {
+        throw Error()
+      }
+      const parsedResponse = await response.json();
+      console.log(parsedResponse)
+      this.setState({
+        boardGame : parsedResponse
+      })
+    } catch (err) {
+      console.log(err, ' this ERROR ALL BOARDGAMES')
+    }
+  }
+
+render(){
   return (
     <div>
-      <h1>Board Game ID PAGE</h1> 
+      <h1>{this.state.boardGame.title}</h1> 
+
     </div>
   )
 }
+} 
 
 export default OneBoardGame;
